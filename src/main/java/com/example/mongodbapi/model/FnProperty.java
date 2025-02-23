c2a42a1fae16e1511c905c423bb9f9fba25e4747
Samarth Munzni <88434740+SamarthMunzni@users.noreply.github.com>
2025-02-23 12:54:04 +0530

    Update FnProperty.java
    collection name

--- src/main/java/com/example/mongodbapi/model/FnProperty.java
+++ src/main/java/com/example/mongodbapi/model/FnProperty.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "fn_properties_data")  // MongoDB collection name
+@Document(collection = "fdb_fn_properties")  // MongoDB collection name
 public class FnProperty {
 
     @Id
