ab3a2d06d558dcadc693813cc008ac84259f47ec
Samarth Munzni <88434740+SamarthMunzni@users.noreply.github.com>
2025-02-23 12:54:39 +0530

    Update Molecule.java
    collection name

--- src/main/java/com/example/mongodbapi/model/Molecule.java
+++ src/main/java/com/example/mongodbapi/model/Molecule.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "molecules_data")
+@Document(collection = "fdb_molecules")
 public class Molecule {
 
     @Id
