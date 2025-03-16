405e2c3146b7dc165d43e22ce8eb7385a1a29a2c
Abhilash Khuntia <58774517+abhilashk23@users.noreply.github.com>
2025-03-16 19:18:34 +0530

    Create HomeController.java
    Added home controller for status check at load balancer

---
+++ src/main/java/com/example/mongodbapi/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.mongodbapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+// @CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/ApiStatus")
+public class HomeController {
+
+    @GetMapping("/Check")
+    public ResponseEntity<String> StatusCheck()
+    {
+       return ResponseEntity.ok("running");
+    }
+}
